a03281ae7ac7b1fa7ab9db638a89a454ca035800
KartikGurung <kartikgurung.cs@gmail.com>
2026-06-25 15:57:43 +0530

    Pyhton day 1

---
+++ pattern.py
@@ -0,0 +1,98 @@
+# 1. Normal Pattern :
+
+# n = 5
+# print("1. Noraml Pattern")
+# for i in range(n):
+#     for j in range(n):
+#         print("*", end=" ")
+#     print()
+
+# 2. Increasing Triangle Pattern:
+
+# n = 5
+#print("2. Increasing Triangle Pattern")
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+# 3. Decreasing Triangle Pattern :
+# n = 5
+# print("3. Decreasing Triangle Pattern")
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*", end=" ")
+#     print()
+
+# 4. Right-Angled Triangle
+
+# n = 5
+# print("4. Right-Angled Triangle")
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end=" ")
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+# 5. Inverted Right-Angled Triangle
+
+# n = 5
+# print("5. Inverted Right-Angled Triangle")
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end = " ")
+#     for j in range(i,n):
+#         print("*", end=" ")
+#     print()
+
+# 6. Pyramid Pattern :
+# n = 5
+# print("6. Pyramid Pattern")
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end=" ")
+
+#     for j in range(i):
+#         print("*", end=" ")
+
+#     for j in range(i+1):
+#         print("*", end = " ")
+#     print()
+
+# 7. Inverse Pyramid Pattern
+# n = 5
+# print("7. Inverse Pyramid Pattern")
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end=" ")
+#     for j in range(i,n):
+#         print("*", end = " ")
+#     for j in range(i, n-1):
+#         print("*", end = " ")
+#     print()
+
+# 8. Daimond Pattern
+# n = 5
+# print("8. Daimond Pattern")
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print(" ", end=" ")
+
+#     for j in range(i):
+#         print("*", end=" ")
+
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end=" ")
+
+#     for j in range(i, n):
+#         print("*", end=" ")
+
+#     for j in range(i, n-1):
+#         print("*", end=" ")
+#     print()
